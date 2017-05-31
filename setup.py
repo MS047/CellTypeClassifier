0e86c507800659304ec508268bbd8f62625ccf83
maximebeau <maximebeaujeanroch047@gmail.com>
2017-05-31 20:07:35 +0100

    NewNameBis

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 # from setuptools import setup
 setup(
-  name = 'CellTypeCLassifier',
-  packages = ['CellTypeCLassifier'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Kilosort/phy generated units supervised and unsupervised classification, with the hope to sort them by cell type.',
+  name = 'XtraDataManager',
+  packages = ['XtraDataManager'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Kilosort/phy generated extracellular data managment, including supervised and unsupervised classification of units, in order to sort them by cell type.',
   author = 'Maxime Beau',
   author_email = 'm.beau047@gmail.com',
-  url = 'https://github.com/MS047/CellTypeClassifier', # use the URL to the github repo
-  download_url = 'https://github.com/MS047/CellTypeClassifier/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/MS047/XtraDataManager', # use the URL to the github repo
+  download_url = 'https://github.com/MS047/XtraDataManager/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['Kilosort', 'Phy', 'Classifier', 'sklearn', 'Extracellular', 'Spike Sorting'], # arbitrary keywords
   classifiers = [],
 )
