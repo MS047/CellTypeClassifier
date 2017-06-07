6b6ea91c4c0f00fa8b581577e3f6470328aef2e2
maximebeau <maximebeaujeanroch047@gmail.com>
2017-06-07 19:36:06 +0100

    Version 0.3

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'XtraDataManager',
   packages = ['XtraDataManager'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'Kilosort/phy generated extracellular data managment, including supervised and unsupervised classification of units, in order to sort them by cell type.',
   author = 'Maxime Beau',
   author_email = 'm.beau047@gmail.com',
   url = 'https://github.com/MS047/XtraDataManager', # use the URL to the github repo
-  download_url = 'https://github.com/MS047/XtraDataManager/archive/0.2.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/MS047/XtraDataManager/archive/0.3.tar.gz', # I'll explain this in a second
   keywords = ['Kilosort', 'Phy', 'Classifier', 'sklearn', 'Extracellular', 'Spike Sorting'], # arbitrary keywords
   classifiers = [],
 )
