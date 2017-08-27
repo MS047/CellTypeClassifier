e639fbbabeea08aee9b0cd956ab9f95c9aa63f36
maximebeau <maximebeaujeanroch047@gmail.com>
2017-08-27 16:50:59 +0100

    v1.1

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ requires=['numpy', 'scipy', 'time', 'progressbar', 'pandas', 'matplotlib', 'dill
 setup(
   name = 'XtraDataManager',
   packages = ['XtraDataManager'], # this must be the same as the name above
-  version = '1.0',
+  version = '1.1',
   description = 'Kilosort/phy generated extracellular data managment, including supervised and unsupervised classification of units, in order to sort them by cell type.',
   author = 'Maxime Beau',
   author_email = 'm.beau047@gmail.com',
   url = 'https://github.com/MS047/XtraDataManager', # use the URL to the github repo
-  download_url = 'https://github.com/MS047/XtraDataManager/archive/0.3.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/MS047/XtraDataManager/archive/1.1.tar.gz', # I'll explain this in a second
   keywords = ['XtraDataManager', 'Phy', 'Classifier', 'sklearn', 'Extracellular', 'Spike Sorting', 'units', 'cell type'], # arbitrary keywords
   classifiers = ['Development Status :: 3 - Alpha', 'Intended Audience :: Science/Research', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.5', 'Environment :: Console', 'Natural Language :: English'],
   install_requires = requires,
